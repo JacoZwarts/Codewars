73606b9578aac6f19ec60d57ac827ad036b59af5
Jaco Zwarts <jaco@zwarts-it.co.za>
2017-05-04 09:24:40 +0200

    VowelCount Kata Finished

---
+++ VowelCount/VowelCouunt/VowelCouunt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelCouunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "aeiou";
+            str = str.ToLower();
+            int a = str.Count(x => x == 'a');
+            int e = str.Count(x => x == 'e');
+            int i = str.Count(x => x == 'i');
+            int o = str.Count(x => x == 'o');
+            int u = str.Count(x => x == 'u');
+
+            int count = a + e + i + o + u;
+            Console.Write(count);
+            Console.Read();
+        }
+    }
+}
