03eb7d1c58af26f2c5b731369b110c57dcc9e725
Jaco Zwarts <jaco@zwarts-it.co.za>
2017-05-05 09:26:31 +0200

    Finished Exes And Ohs Kata

---
+++ ExesAndOhs/ExesAndOhs/ExesAndOhs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExesAndOhs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "ooom";
+
+            var list = new List<string>();
+            foreach (var letter in input)
+            {
+                list.Add(letter.ToString().ToLower());
+            }
+
+            var xCount = list.Count(letter => letter == "x");
+            var oCount = list.Count(letter => letter == "o");
+
+            if(xCount == oCount)
+                Console.Write("True");
+            else
+                Console.Write("False");
+
+            Console.Read();
+        }
+    }
+}
